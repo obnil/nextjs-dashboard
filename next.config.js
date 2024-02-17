27201ec0bb91070ffb7913ea9f1c136b68b76ce7
namilin <namilin@icloud.com>
2024-02-17 15:57:00 +0800

    fix Auth failed direct to undefined 404 MissingSecret

--- next.config.js
+++ next.config.js
@@ -1,4 +1,16 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+// const nextConfig = {};
 
-module.exports = nextConfig;
+// module.exports = nextConfig;
+
+module.exports = {
+  reactStrictMode: true,
+  swcMinify: true,
+  env: {
+    mongodb_username: 'USERNAME',
+    mongodb_password: 'PASSWORD',
+    mongodb_db: 'DB',
+    NEXTAUTH_URL: 'http://localhost:3000/api/auth',
+    NEXTAUTH_SECRET: 'oaLiqG+PIwZXL3zZ9VbEMRM/3NocWo7iT68BICEbMdU=',
+  },
+};
